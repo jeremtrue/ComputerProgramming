6d247ec1cbc6e64b9c3d492740fbd43397e01bf3
jeremtrue <jeremyptruelove788@gmail.com>
2023-11-15 22:00:48 +0000

    needing to fix this

--- ComputerProgrammingProject/classes.py
+++ ComputerProgrammingProject/classes.py
@@ -1,25 +1,23 @@
 import os
 import random as rand
 
+
 class Game:
     def __init__(self):
         self.player = self.Player()
     
     class Initialize:
         def __init__(self):
-            self.situation1 = Game.Initialize.getphrases(situation1)
             self.name = input("What is your characters name?")
-            #print('WARNING: Saves are in development dont rely on them. Just say no if no save')
-            #self.ifsave = input("Do you have a save file?")
-            Game.Start(self.name)
-        def getphrases(x):
+            self.getphrases()
+            Game.Start()
+        def getphrases(self):
             idk1 = ['Gorlock The Destoryer', 'Bob the Destoryer', 'Totaly Inncent Person']
             idk2 = ['You awaken in a dark cave.',
                     'You wake up in the back of a wagon.',
                     'You burst awake to find yourself in the middle of a dark room.']
-            monstername = rand.choice(idk1)
-            situation1 = rand.choice(idk2)
-
+            self.monstername = rand.choice(idk1)
+            self.situation1 = rand.choice(idk2)
 
     class Save:
         class Exists:
@@ -30,8 +28,7 @@ class Game:
             def __init__(self):
                 pass #CHANGE THIS
 
-
-    class Player():
+    class Player:
         def __init__(self):
             self.health = 0
             self.weapons = []
@@ -41,9 +38,5 @@ class Game:
             self.level = 0
     
     class Start:
-        
-
-
-
-            
-
+        def gamefunc(self):
+            print(self.Initialize.situation1)
