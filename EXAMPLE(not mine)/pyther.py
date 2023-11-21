b2a2d1cd69035d5941b853cdce323a7c57deadfb
jeremtrue <jeremyptruelove788@gmail.com>
2023-11-20 18:32:13 -0800

    commititing this

---
+++ EXAMPLE(not mine)/pyther.py
@@ -0,0 +1,10 @@
+import classes
+            
+Game = classes.MainGame
+import classes
+
+player = classes.Player
+player = Game.InitializePlayer(player)
+print(Game.woodensword)
+Game.MainGameFunc(player, Game)
+
