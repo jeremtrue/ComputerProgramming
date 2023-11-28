323274186bff3366fd6de5f28b0e00bbcd0ca8b6
jeremtrue <jeremyptruelove788@gmail.com>
2023-11-27 19:14:18 -0800

    cleaning some things up

--- ComputerProgrammingProject/classes.py
+++ ComputerProgrammingProject/classes.py
@@ -1,56 +1,88 @@
-import os
+import json
 import random as rand
-
+import os
 
 class Game:
     def __init__(self):
-        self.player = self.Player()
+        pass
 
-    
-    class Initialize:
-        def __init__(self):
-            self.name = input("What is your characters name?")
-            phrases = self.getphrases()
-            enename = self.getnames()
+    def printdir(self):
+        print('\nHow to play:')
+        print('First, you would enter your name')
+        print('Next, you would start your journey.')
+        print('When you are asked a yes or no question, just type y/n or yes/no.')
+        print('When you are asked to input a number from a chart, just type the number next to your answer.\n')
+        firstque = input('Do you understand? y/n: ')
+        if firstque == 'y' or firstque == 'yes':
+            print('Great! Good luck')
+        else:
+            print('Well then, you see, I have no clue. Good luck!\n')
 
-        def getphrases(self):
-            idk2 = ['You awaken in a dark cave.',
-                    'You wake up in the back of a wagon.',
-                    'You burst awake to find yourself in the middle of a dark room.']
-            self.situation1 = rand.choice(idk2)
-        def getnames(self):
-             idk1 = ['Gorlock The Destoryer', 'Bob the Destoryer', 'Totally Inncent Person']
-             self.monstername = rand.choice(idk1)
+class Initialize:
+    def __init__(self):
+        # Instantiate the Game class
+        self.game = Game()
+        self.dirque()
+        self.name = self.getcharname()
 
-    class Save:
-        def __init__(self):
-            self.exists = self.Exists()
-            self.first = 0
-        class Exists:
-            def __init__(self):
-                self.first = 0 
-                self.health = 0
-                self.damage = 0
-                self.level = 0
-                self.enelost = 0
-                self.enewon = 0
-                self.inventory = 0
+    def getcharname(self):
+        name = str(input("What is your Character's name? "))
+        return name
 
-        class Load:
-            def __init__(self):
-                pass #CHANGE THIS
+    def getphrases(self):
+        idk2 = ['You awaken in a dark cave.',
+                'You wake up in the back of a wagon.',
+                'You burst awake to find yourself in the middle of a dark room.']
+        return rand.choice(idk2)
 
-    class Player:
-        def __init__(self):
-            self.health = 0
-            self.weapons = []
-            self.inventory = {}
-            self.movementspeed = 1
-            self.points = 0
-            self.level = 0
-    
- #   class Start:
-  #      def __init__(self):
-  #          Game.Initialize.getphrases()
-   #     def gamefunc():
- #           print(Game.Initialize.situation1)
+    def getnames(self):
+        idk1 = ['Gorlock The Destroyer', 'Bob the Destroyer', 'Totally Innocent Person']
+        return rand.choice(idk1)
+
+    def dirque(self):
+        firstque = input('\nDo you need instructions? y/n: ')
+        if firstque == 'y' or firstque == 'yes':
+            self.game.printdir()
+        else:
+            print('\nWell then, you see, I think you need them!')
+            self.game.printdir()
+
+class Save:
+    def __init__(self):
+        self.damage = 0
+        self.first = 1
+        self.health = 0
+        self.level = 0
+        self.enelost = 0
+        self.enewon = 0
+        self.inventory = []
+        self.levelinprog = 0
+        self.name = ""
+
+class Saves:
+    def __init__(self):
+        pass
+
+    filename = 'save.json'
+
+    def load(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+
+                name = data['character']['name']
+                health_level = data['character']['health']
+                damage = data['character']['damage']
+                enelost = data['character']['combat']['enelost']
+                enewon = data['character']['combat']['enewon']
+                inventory = data['character']['inventory']['inventory']
+                return name, health_level, damage, enelost, enewon, inventory
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            return None, None, None, None, None, None
+        except Exception as e:
+            print(f"Error loading data: {e}")
+            return None, None, None, None, None, None
+
+saves = Saves()
+#name, health_level, damage, enelost, enewon, inventory = saves_instance.load(filename)
--- ComputerProgrammingProject/game.py
+++ ComputerProgrammingProject/game.py
@@ -1,21 +1,5 @@
-from classes import Game
-game = Game()
-game.save = Game.Save()
-#game.Save.Load
-game.Initialize()
-if game.save.exists.first == 0:
-    def printdir():
-        print('How to play:')
-        print('First you would enter your name')
-        print('Next you would start your journey.')
-        print('When you are asked a yes or no questing just type y/n of yes/no.')
-        print('When you are asked to input a number from a chart, just type the number next to your asnwer.')
-        firstque = input('Do you understand? y/n')
-        if firstque == 'y' or firstque == 'yes':
-            print('Great! good luck')
-        else:
-            print('Well then, you see, I have no clue. good luck!')
-    printdir()
-
-    
+#import
+import classes
+#RUN IT
+classes.Initialize()
     
